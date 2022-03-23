ed9689a998bbab2ed31ef63c632e43047c584335
arsyanmw <arsyanmw12@gmail.com>
2022-03-24 02:29:28 +0700

    update: add components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
 import {FAB} from './FAB';
 import {TouchableCard} from './TouchableCard';
 import {Button} from './Button';
+import {CardDetails} from './Card';
+import {Header} from './Header';
+import {LoadingScreen} from './Loading';
 
-export {FAB, TouchableCard, Button};
+export {FAB, TouchableCard, Button, CardDetails, Header, LoadingScreen};
