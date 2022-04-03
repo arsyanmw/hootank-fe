479ae49b0a4f94695e36e39058e449e0d69a0ea9
arsyanmw <arsyanmw12@gmail.com>
2022-04-03 15:51:51 +0700

    update: change loading display

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,19 +1,46 @@
 import {Text, View} from 'react-native';
 import LottieView from 'lottie-react-native';
-import loading from '../../assets/lottie/loading.json';
+import getData from '../../assets/lottie/getData.json';
+import ask from '../../assets/lottie/ask.json';
+import waiting from '../../assets/lottie/waiting.json';
 import React from 'react';
 
 export const LoadingScreen = () => {
+  const loadingText = [
+    {
+      title: 'Tunggu Sebentar ya..',
+      icon: waiting,
+      style: {
+        height: 120,
+      },
+    },
+    {
+      title: 'Mengambil data hutang..',
+      icon: getData,
+      style: {
+        height: 100,
+      },
+    },
+    {
+      title: 'Siapa aja ya yang belum bayar?..',
+      icon: ask,
+      style: {
+        height: 80,
+      },
+    },
+  ];
+  const random = Math.floor(Math.random() * loadingText.length);
+
   return (
     <View style={{height: '100%'}}>
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <LottieView
-          source={loading}
+          source={loadingText[random].icon}
           autoPlay
-          style={{width: '50%', height: 80}}
+          style={[{width: '80%'}, loadingText[random].style]}
         />
         <Text style={{fontWeight: 'bold', color: '#1C658C'}}>
-          Tunggu Sebentar..
+          {loadingText[random].title}
         </Text>
       </View>
     </View>
